55bf9cdf82077db00911c7856ea5b96a6ba0bfa3
Rodrigo Olave Carrasco <rholave@gmail.com>
2018-08-31 01:08:21 -0300

    cambios para heroku

--- TrilliBiTheme/config/initializers/assets.rb
+++ TrilliBiTheme/config/initializers/assets.rb
@@ -8,21 +8,15 @@ Rails.application.config.assets.version = '1.0'
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'bootstrap', 'css')
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'bootstrap', 'fonts')
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'bootstrap', 'js')
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'css')
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'elegant-font')
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'font-awesome', 'css')
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'font-awesome', 'fonts')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'font-awesome')
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'ico')
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'img')
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'img', 'backgrounds')
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'img', 'modal')
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'img', 'screenshots')
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'img', 'team')
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'img', 'testimonials')
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'js')
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'ultimate-flat-social-icons')
+
 
 Rails.application.config.assets.precompile += %w( *.js *.css *.jpg *.png *.gif )
 # Precompile additional assets.
